67c287d58e35dfbf263ff03266dadbc5015b39fa
MiraJuwita <98795964+MiraJuwita@users.noreply.github.com>
2022-02-06 15:00:11 +0700

    Update G1A021056_Mira Juwita Ali_ADT.cpp

--- G1A021056_Mira Juwita Ali_ADT.cpp
+++ G1A021056_Mira Juwita Ali_ADT.cpp
@@ -6,9 +6,9 @@ int main()
     Trapesium t1;
     tr=Trapesium(6,4,5);
 //ubahTrapesium(&t1,6, 5,7);
-    printf("Sisi: %d",t1.s1);
+    printf("Sisi1: %d",t1.s1);
     printf("\n");
-    printf("Sisi: %d",t1.s2);
+    printf("Sisi2: %d",t1.s2);
     printf("\n");
     printf("Tinggi: %d",t1.t);
     printf("\n");
